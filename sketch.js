aaa0be8c52fe39eb16f8934a7b93261e84fb082f
unknown <skasraf001@gmail.com>
2020-11-10 16:58:07 +0530

    synchronousBallMovement

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-var ball;
+var ball,position,database;
 
 function setup(){
     createCanvas(500,500);
+
+database = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database.ref('ball/position').on("value",readPos,showErr);
+}
+function readPos(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showErr(){
+    console.log("Error while reading the code");
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x:position.x+x,
+        y:position.y+y
+    })
+
 }
